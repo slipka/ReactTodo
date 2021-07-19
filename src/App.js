64854493acf49f92da8336439cd32eb71b29cba2
yuraBarykin <barykin.yuriy@b-leader.ru>
2021-07-19 22:43:24 +0400

    стили для aside блока

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import sprite from './icon/sprite.svg'
+
 
 function App() {
   return (
     <main className="main">
 			<aside className="aside">
 				<div className="aside__wrapper">
-					<h1 className="aside__title">
-						<span className="aside__title-icon"></span>
+					<button className="aside__button">
+						<span className="aside__button-icon">
+							<svg className="aside__button-svg" width="12px" height="10px">
+								<use href={ sprite + "#todo" } />
+							</svg>
+						</span>
 						Все задачи
-					</h1>
+					</button>
+					<ul className="list">
+						<li className="list__item">
+							<button className="list__button">Покупки</button>
+						</li>
+						<li className="list__item"><button className="list__button">Фронтенд</button></li>
+						<li className="list__item"><button className="list__button">Фильмы и сериа...</button></li>
+						<li className="list__item"><button className="list__button">Книги</button></li>
+						<li className="list__item"><button className="list__button">Личное</button></li>
+					</ul>
 				</div>
 			</aside>
 			<section className="body">body</section>
